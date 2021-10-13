2a6b6e54067dc5b953b3f14fc53cbe001619611e
Archana <archanasurendran12@gmail.com>
2021-10-13 22:24:55 +0530

    new project

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb Works</p>
+                <p>Newroom</p>
+                <p>Airbnb plus</p>
+                <p>Airbnb luxe</p>
+                <p>Investor</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>Accessibility</p>
+                <p>Newroom</p>
+                <p>Airbnb plus</p>
+                <p>Airbnb luxe</p>
+                <p>Investor</p>
+        </div>
+        <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb Works</p>
+                <p>Newroom</p>
+                <p>Airbnb plus</p>
+                <p>Airbnb luxe</p>
+                <p>Investor</p>
+                </div>
+                <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>Accessibility</p>
+                <p>Newroom</p>
+                <p>Airbnb plus</p>
+                <p>Airbnb luxe</p>
+                <p>Investor</p>
+                </div>
+                </div>
+    )
+}
+
+export default Footer
